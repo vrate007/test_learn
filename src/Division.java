0e5eea57f33d6a6ccde7fef44855cb4fc1b1724e
dn240389kvv <vladislav.krivich@privatbank.ua>
2025-08-08 11:32:32 +0300

    task_2

--- src/Division.java
+++ src/Division.java
@@ -1,7 +1,7 @@
 class Division implements MathFunction {
     public double calculate(double a, double b) {
         if (b == 0) {
-            System.out.println("Error: Division by zero is not allowed.");
+            System.out.println("Помилка: Ділення на 0 неможливе.");
             return Double.NaN;
         }
         return a / b;
